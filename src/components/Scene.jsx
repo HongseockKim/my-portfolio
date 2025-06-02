ce65b02577313d3d834150b0c6c805088368f9e2
mrHong <hongseok.k@ingstory.kr>
2025-06-02 15:46:18 +0900

    포토폴리오

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -4,19 +4,15 @@ function Scene() {
 
     return (
         <>
-            <mesh visible userData={{ hello: 'world' }} position={[1, 2, 3]} rotation={[Math.PI / 2, 0, 0]}>
-              <sphereGeometry args={[1, 16, 16]} />
-              <meshStandardMaterial color="hotpink" transparent />
-            </mesh>
                   <mesh
-                      position={[0, -1, 0]}
+                      position={[0, -8, 0]}
                       rotation={[-Math.PI / 2, 0, 0]}
                       receiveShadow
                   >
-                <planeGeometry args={[20, 20]} />
-                <meshStandardMaterial color="#f0f0f0" />
+                <planeGeometry args={[100, 20,20,30]} />
+                <meshStandardMaterial color="#fff" />
             </mesh>
-            <ambientLight intensity={0.4} />
+            <ambientLight intensity={0.1} />
             <directionalLight
                 position={[10, 10, 5]}
                 intensity={1}
@@ -24,6 +20,7 @@ function Scene() {
                 shadow-mapSize-width={2048}
                 shadow-mapSize-height={2048}
             />
+            <fog attach="fog" args={['#fff', 0, 100]} />
         </>
 
     );
