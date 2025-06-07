0a6182d38ed7a2b2fcf868c5a366fbb7028217e7
redStoneKim <hongseok0610@gmail.com>
2025-06-07 20:17:46 +0900

    모델링추가

--- src/components/PortThree.jsx
+++ src/components/PortThree.jsx
@@ -3,19 +3,19 @@ import Masonry from 'react-masonry-css';
 const PortThree = () => {
     const BASE_URL = import.meta.env.BASE_URL;
     const items = [
-        { id: 1, image: `smeg.png`, title: 'smeg' },
-        { id: 2, image: `${BASE_URL}daelim.png`, title: '대림 코퍼레이션' },
-        { id: 3, image: `${BASE_URL}children.png`, title: '육아정책연구소' },
-        { id: 4, image: `${BASE_URL}canvas.png`, title: 'canvas' },
-        { id: 5, image: `${BASE_URL}sympol.png`, title: 'liveto' },
-        { id: 6, image: `${BASE_URL}ugly.png`, title: '어글리스토브' },
-        { id: 7, image: `${BASE_URL}seemple.png`, title: '심플' },
-        { id: 8, image: `${BASE_URL}bankx2.png`, title: '뱅크엑스' },
-        { id: 9, image: `${BASE_URL}tankyoy.png`, title: '땡큐마켓' },
-        { id: 10, image: `${BASE_URL}haru.png`, title: '하루세탁' },
-        { id: 11, image: `${BASE_URL}sild.png`, title: '실드' },
-        { id: 11, image: `${BASE_URL}pickko.png`, title: '픽코앱' },
-        { id: 12, image: `${BASE_URL}pickko_kisok.png`, title: '키오스크' },
+        { id: 1, image: `img/smeg.png`, title: 'smeg' },
+        { id: 2, image: `img/daelim.png`, title: '대림 코퍼레이션' },
+        { id: 3, image: `img/children.png`, title: '육아정책연구소' },
+        { id: 4, image: `img/canvas.png`, title: 'canvas' },
+        { id: 5, image: `img/sympol.png`, title: 'liveto' },
+        { id: 6, image: `img/ugly.png`, title: '어글리스토브' },
+        { id: 7, image: `img/seemple.png`, title: '심플' },
+        { id: 8, image: `img/bankx2.png`, title: '뱅크엑스' },
+        { id: 9, image: `img/tankyoy.png`, title: '땡큐마켓' },
+        { id: 10, image: `img/haru.png`, title: '하루세탁' },
+        { id: 11, image: `img/sild.png`, title: '실드' },
+        { id: 11, image: `img/pickko.png`, title: '픽코앱' },
+        { id: 12, image: `img/pickko_kisok.png`, title: '키오스크' },
     ];
 
     const breakpoints = {
